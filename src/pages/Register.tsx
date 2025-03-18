bf6be68cc39573dbeb13dfad6494b8953b8e8c3f
Raineetu <raineetu0070@gmail.com>
2025-03-18 22:57:20 +0545

    added register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,17 +3,28 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Button } from "@/components/ui/button";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <Card className="w-full max-w-sm p-6 shadow-md bg-white rounded-lg">
         <CardHeader className="text-center">
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>Enter your info to log in</CardDescription>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>Enter your info to Register</CardDescription>
         </CardHeader>
 
         <CardContent>
           <form className="space-y-4">
+            <div className="flex gap-3">
+                <div>
+                    <Label htmlFor="fname">First Name</Label>
+                    <Input type="text" id="fname" placeholder="First name" required />
+                </div>
+                <div>
+                    <Label htmlFor="lname">Last Name</Label>
+                    <Input type="text" id="lname" placeholder="Last name" required />
+                </div>
+            </div>
+
             <div className="flex flex-col gap-2">
               <Label htmlFor="email">Email</Label>
               <Input type="email" id="email" placeholder="email@example.com" required />
@@ -25,18 +36,18 @@ const Login = () => {
             </div>
 
             <Button type="submit" className="w-full">
-              Login
+              Register
             </Button>
 
             <Button variant="outline" className="w-full">
-              Login with Google
+              Register with Google
             </Button>
           </form>
 
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
-            <a href="/register" className="underline text-blue-600 hover:text-blue-800">
-              Register
+            Already have an account?{" "}
+            <a href="/login" className="underline text-blue-600 hover:text-blue-800">
+              Login
             </a>
           </div>
         </CardContent>
@@ -45,4 +56,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "@/pages/Login";
 import HomePage from "@/pages/HomePage";
+import Register from "@/pages/Register";
 
 const router =  createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router =  createBrowserRouter([
     {
         path: "/login",
         element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
     }
 ])
 export default router
